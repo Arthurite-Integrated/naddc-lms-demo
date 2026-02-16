7d96b364d3c9e3e85322c5abf1c48d460389dace
Paul Aderoju <paul.adeboye@yahoo.com>
2026-02-16 16:29:16 +0100

    fix: minor changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,13 +4,13 @@ import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import { SidebarProvider } from "@/components/ui/sidebar"
 import { DashboardSidebar } from "@/components/dashboard-sidebar"
+import Image from "next/image"
 
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata = {
   title: "National Directorate of Employment LMS",
   description: "Learning Management System for the National Directorate of Employment",
-    generator: 'v0.dev'
 }
 
 export default function RootLayout({
@@ -27,8 +27,20 @@ export default function RootLayout({
               <DashboardSidebar />
               <main className="flex-1 overflow-y-auto">
                 {children}
+
                 <footer className="border-t py-4 px-6 text-center text-sm text-gray-500">
-                  © {new Date().getFullYear()} National Directorate of Employment. Powered by Arthurite Integrated.
+                  <div className="flex flex-col md:flex-row gap-2 justify-center items-center">
+                    <div> © {new Date().getFullYear()} National Directorate of Employment. Powered by <span className="font-semibold text-green-800"><a href="https://www.arthuriteintegrated.com" target="_blank" rel="noopener noreferrer">Arthurite Integrated</a></span></div>
+                    <div>
+                      <Image
+                      src="https://atr-logo.s3.us-east-1.amazonaws.com/atr-logo.png"
+                      alt="Arthurite Logo"
+                      width={24}
+                      height={24}/>
+                    </div>
+                  </div>
+                 
+                    
                 </footer>
               </main>
             </div>
@@ -39,5 +51,3 @@ export default function RootLayout({
   )
 }
 
-
-import './globals.css'
